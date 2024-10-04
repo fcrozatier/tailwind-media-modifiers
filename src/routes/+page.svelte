19efff17fccf0c56bbb351c8d0a086487e651a17
fcrozatier <frederic.crozatier@protonmail.com>
2024-10-04 17:30:08 +0200

    add prose

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,16 @@
 	import Component from './Component.svelte';
 </script>
 
+<!-- @utility can be overridden -->
 <p class="flex justify-center gap-4 pt-20">
-	<Component class="btn-sm rounded-none"></Component>
 	<Component class="btn-sm"></Component>
+	<Component class="btn-sm rounded-none"></Component>
 </p>
+
+<article class="prose mx-auto">
+	<!-- Default red text -->
+	<p>I'm red</p>
+
+	<!-- Can be overridden when importing prose in plugins layer -->
+	<p class="text-blue-500">If I'm blue it's working</p>
+</article>
