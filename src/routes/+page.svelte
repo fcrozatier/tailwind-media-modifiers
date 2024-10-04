12c7d67f18766adf80de0f862ae02a058d467ca9
fcrozatier <frederic.crozatier@protonmail.com>
2024-10-04 14:54:30 +0200

    demo

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import Component from './Component.svelte';
+</script>
+
+<p class="flex justify-center gap-4 pt-20">
+	<Component class="btn-sm md:[.btn-lg] allowed"></Component>
+	<Component class="btn-sm md:[.btn-lg]"></Component>
+</p>
---
+++ src/routes/Component.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	let { class: className }: { class?: string } = $props();
+</script>
+
+<button class={'not-[.allowed]:text-red-500 ' + className}>Hey</button>
+
+<style>
+	/* Whether layered or not, defined here or in app.css responsive classes do not work */
+	/* @layer components {
+		.btn-sm {
+			padding: var(--spacing-1);
+			background-color: var(--color-indigo-500);
+			color: var(--color-gray-700);
+		}
+
+		.btn-lg {
+			padding: var(--spacing-10);
+			background-color: var(--color-red-500);
+			color: var(--color-blue-700);
+		}
+	} */
+</style>
