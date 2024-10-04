d5e05258d3ae471de844e62729a41f44439a2918
fcrozatier <frederic.crozatier@protonmail.com>
2024-10-04 17:12:20 +0200

    add @utilities

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,6 @@
 </script>
 
 <p class="flex justify-center gap-4 pt-20">
-	<Component class="btn-sm md:[.btn-lg] allowed"></Component>
-	<Component class="btn-sm md:[.btn-lg]"></Component>
+	<Component class="btn-sm rounded-none"></Component>
+	<Component class="btn-sm"></Component>
 </p>
